77da724c63e86c4ee145f7648c1aa03a852a787d
Sam <Sam@Dan>
2018-10-15 18:26:15 +0300

    Still trying use Git!

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+from flask import Flask
+from config import basedir
+from peewee import *
+from playhouse.flask_utils import FlaskDB
+
+app = Flask('server',template_folder='frontend/templates',static_folder='frontend/static')
+
+app.config.from_object('server.config')
+
+db = FlaskDB(app)
+
+from server.app import models,auth,api,views
